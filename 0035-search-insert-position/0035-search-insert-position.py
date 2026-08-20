31d74076a46dc8fc02bd912f6f5ea7a76a833c4a
Janani D H <jannidh007@gmail.com>
2026-08-20 21:13:56 +0530

    Time: 21 ms (0.84%), Space: 12.9 MB (66.18%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return left            
+                
+
+        
+                
+                    
+               
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
