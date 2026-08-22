e034ea8d349d07b3e8d3baf505b33a28820ab76a
Janani D H <jannidh007@gmail.com>
2026-08-22 23:03:52 +0530

    Time: 0 ms (100%), Space: 12.3 MB (59.07%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        num1=int(a,2)
+        num2=int(b,2)
+        add=num1+num2
+        return bin(add)[2:]
+        
+         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
